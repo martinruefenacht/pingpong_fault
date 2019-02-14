d39184e160c847c353691e83c283753d8edcaea5
Martin Ruefenacht <martin-ruefenacht@utc.edu>
2019-02-14 15:49:54 -0500

    Added ignore and simple changes

--- submit_local_simple.sh
+++ submit_local_simple.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-time mpiexec -n 2 --separate_rank --separate_error --logexec --logfault --loghead --logcontrol ./pingpong
+time mpiexec -n 2 --separate_rank --separate_error --logexec --logfault --loghead --logcontrol ./pingpong_perf
